156084717c269a2f3ce029136079ff693247968a
Osos0 <osama_magdy0@yahoo.com>
2024-04-11 10:08:58 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+function App() {
+  return (
+    <div className="App">
+      <div>APP</div>
+    </div>
+  );
+}
+
+export default App;
