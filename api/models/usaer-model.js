10e0624b455faacd2376a92a985e18f96cf7084c
Osos0 <osama_magdy0@yahoo.com>
2024-04-11 20:48:20 +0200

    Add user scheme

---
+++ api/models/usaer-model.js
@@ -0,0 +1,33 @@
+// const mongoose = require("mongoose");
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    },
+  },
+  { timestamps: true }
+);
+
+// member = name of the data in mongoosedb
+const User = mongoose.model("User", UserSchema);
+
+export default User;
+// module.exports = User;
