5f204ac6f9980225ac37f1335df69977bafae86e
Osos0 <osama_magdy0@yahoo.com>
2024-04-11 20:38:46 +0200

    connect mongo db

--- api/index.js
+++ api/index.js
@@ -1,24 +1,23 @@
 import express from "express";
-// import mongoose from "mongoose";
-// import dotenv from "dotenv";
-// import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
 
-// dotenv.config();
+dotenv.config();
 
 const app = express();
 
 // app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 
 // app.use(cookieParser());
 
 const PORT = 5000;
 
-// mongoose
-//   .connect(process.env.MONGO)
-//   .then(() => console.log("Connected to MongoDB"));
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
