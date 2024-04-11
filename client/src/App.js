93c1daec3ef64b245210545d8dab56c3c98c4f27
Osos0 <osama_magdy0@yahoo.com>
2024-04-11 10:31:30 +0200

    Add routes in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import Home from "./Pages/Home";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/Signup";
+import Profile from "./Pages/Profile";
+
 function App() {
   return (
     <div className="App">
-      <div>APP</div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
