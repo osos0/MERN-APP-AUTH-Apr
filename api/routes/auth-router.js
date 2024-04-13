9c31e16690f01c2dc757e6f2e8a7123baefc54a6
Osos0 <osama_magdy0@yahoo.com>
2024-04-13 20:59:09 +0200

    functionalty of signup

--- api/routes/auth-router.js
+++ api/routes/auth-router.js
@@ -3,6 +3,6 @@ import Singup from "../conrollers/auth-controller.js";
 
 const router = exprees.Router();
 
-router.post("/", Singup);
+router.post("/signup", Singup);
 
 export default router;
--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,7 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
+// import axios from "axios";
 
-const Signup = () => {
-  return <div>Signup</div>;
+const Singup = () => {
+  const [valueSignup, setValueSignup] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  // creating an object to post it in mongo DB
+  const handelsignvalue = (e) => {
+    setValueSignup({ ...valueSignup, [e.target.id]: e.target.value });
+  };
+
+  const handelSubmite = async (e) => {
+    e.preventDefault();
+
+    /////////////////////////////////////////////////
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("http://localhost:5000/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(valueSignup),
+      });
+      const data = await res.json();
+
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+    /////////////////////////////////////////////////
+    //   try {
+    //     setLoading(true);
+    //     await axios.post("http://localhost:5000/api/user/signup", valueSignup);
+    //     setLoading(false);
+    //     setError(false);
+    //     alert("User created successful");
+    //   } catch (error) {
+    //     setLoading(false);
+    //     setError(false);
+    //     alert("Somthing went wrong or maybe Username or Email unavilabel");
+    //   }
+  };
+
+  return (
+    <>
+      <h3>Sing Up</h3>
+      <form onSubmit={handelSubmite}>
+        <input
+          id="username"
+          type="text"
+          placeholder="Username"
+          onChange={handelsignvalue}
+        />
+        <input
+          id="email"
+          type="email"
+          placeholder="Email"
+          onChange={handelsignvalue}
+        />
+        <input
+          id="password"
+          type="password"
+          placeholder="Password"
+          onChange={handelsignvalue}
+        />
+        <button disabled={loading}>{loading ? "Loading..." : "Sign Up"}</button>
+        <button type="button" className="btnOfGoogle">
+          CONTINUE WITH GOOGLE
+        </button>
+      </form>
+      <div className="d-flex p-4 ">
+        <div>Have an account ?..</div>
+        <Link to="/signin">. Sign In</Link>
+      </div>
+      {error && (
+        <div className="alert alert-danger" role="alert">
+          {error && " Somethings Went Wrong"}
+        </div>
+      )}
+    </>
+  );
 };
 
-export default Signup;
+export default Singup;
