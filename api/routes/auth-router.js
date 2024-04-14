1ed22cbb93c46f2b93da524ca7405a6df2c43677
Osos0 <osama_magdy0@yahoo.com>
2024-04-14 11:30:09 +0200

    create signin api route

--- api/routes/auth-router.js
+++ api/routes/auth-router.js
@@ -1,8 +1,9 @@
 import exprees from "express";
-import Singup from "../conrollers/auth-controller.js";
+import { Singup, Singin } from "../conrollers/auth-controller.js";
 
 const router = exprees.Router();
 
 router.post("/signup", Singup);
+router.post("/signin", Singin);
 
 export default router;
