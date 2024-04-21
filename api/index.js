1750892866d34b0e62cb267e75fca56344b16b64
Osos0 <osama_magdy0@yahoo.com>
2024-04-21 18:39:42 +0200

    fixing

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/public/dist")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
