5c381e871c223a455d79a7e9d85e74843e7d9ccd
Osos0 <osama_magdy0@yahoo.com>
2024-04-21 17:08:03 +0200

    added lines codes for production deployment

--- api/index.js
+++ api/index.js
@@ -6,9 +6,17 @@ import userRoute from "../api/routes/user-routes.js";
 import authRoute from "../api/routes/auth-router.js";
 // import { error } from "console";
 import cookieParser from "cookie-parser";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(express.json());
 
