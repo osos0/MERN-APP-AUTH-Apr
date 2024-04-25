7250fa758877e71c4517c1585aa68087efb09660
Osos0 <osama_magdy0@yahoo.com>
2024-04-25 09:50:59 +0200

    fixing bug in frontend side

--- api/index.js
+++ api/index.js
@@ -13,9 +13,10 @@ const __dirname = path.resolve();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/public")));
+
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
 app.use(express.json());
